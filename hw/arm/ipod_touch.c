a6c3de5e45f98782e6793f4e6fdb4a8d5332b546
Martijn de Vos <mdmartijndevos@gmail.com>
2021-12-26 13:27:04 +0100

    Cleaned up

--- hw/arm/ipod_touch.c
+++ hw/arm/ipod_touch.c
@@ -7,11 +7,9 @@
 #include "hw/irq.h"
 #include "sysemu/sysemu.h"
 #include "sysemu/reset.h"
-#include "hw/arm/xnu.h"
 #include "hw/platform-bus.h"
 #include "hw/block/flash.h"
 #include "hw/qdev-clock.h"
-#include "hw/arm/xnu_mem.h"
 #include "hw/arm/ipod_touch.h"
 #include "hw/arm/ipod_touch_spi.h"
 #include "hw/arm/exynos4210.h"
@@ -65,6 +63,18 @@
 #define KERNELCACHE_BASE 0x9000000
 #define FRAMEBUFFER_MEM_BASE 0xfe00000
 
+static void allocate_ram(MemoryRegion *top, const char *name, uint32_t addr, uint32_t size)
+{
+        MemoryRegion *sec = g_new(MemoryRegion, 1);
+        memory_region_init_ram(sec, NULL, name, size, &error_fatal);
+        memory_region_add_subregion(top, addr, sec);
+}
+
+static uint32_t align_64k_high(uint32_t addr)
+{
+    return (addr + 0xffffull) & ~0xffffull;
+}
+
 static void ipod_touch_cpu_setup(MachineState *machine, MemoryRegion **sysmem, ARMCPU **cpu, AddressSpace **nsas)
 {
     Object *cpuobj = object_new(machine->cpu_type);
@@ -91,7 +101,6 @@ static void ipod_touch_cpu_reset(void *opaque)
     IPodTouchMachineState *nms = IPOD_TOUCH_MACHINE((MachineState *)opaque);
     ARMCPU *cpu = nms->cpu;
     CPUState *cs = CPU(cpu);
-    CPUARMState *env = &cpu->env;
 
     cpu_reset(cs);
 
@@ -442,7 +451,6 @@ static void lcd_invalidate(void *opaque)
 
 static void draw_line32_32(void *opaque, uint8_t *d, const uint8_t *s, int width, int deststep)
 {
-    uint16_t v;
     uint8_t r, g, b;
 
     do {
@@ -562,51 +570,8 @@ static void ipod_touch_init_sysic(MachineState *machine, MemoryRegion *sysmem)
 
 static void ipod_touch_memory_setup(MachineState *machine, MemoryRegion *sysmem, AddressSpace *nsas)
 {
-    uint32_t kernel_low;
-    uint32_t kernel_high;
-    uint32_t virt_base;
-    uint32_t phys_ptr;
-    uint32_t phys_pc;
-    uint64_t dtb_size;
     IPodTouchMachineState *nms = IPOD_TOUCH_MACHINE(machine);
     DriveInfo *dinfo;
-
-    // macho_file_highest_lowest_base(nms->kernel_filename, IPOD_TOUCH_PHYS_BASE, &virt_base, &kernel_low, &kernel_high);
-
-    // g_virt_base = virt_base;
-    // g_phys_base = IPOD_TOUCH_PHYS_BASE;
-    // phys_ptr = IPOD_TOUCH_PHYS_BASE;
-
-    // printf("Virt base: %08x, kernel lowest: %08x, kernel highest: %08x\n", virt_base, kernel_low, kernel_high);
-
-    // //now account for the loaded kernel
-    // arm_load_macho(nms->kernel_filename, nsas, sysmem, "kernel.n45", IPOD_TOUCH_PHYS_BASE, virt_base, kernel_low, kernel_high, &phys_pc);
-    // nms->kpc_pa = phys_pc; // TODO unused for now
-
-    // phys_ptr = align_64k_high(kernel_high);
-    // uint32_t kbootargs_pa = phys_ptr;
-    // nms->kbootargs_pa = kbootargs_pa;
-    // phys_ptr += align_64k_high(sizeof(struct xnu_arm_boot_args));
-    // printf("Loading bootargs at memory location %08x\n", nms->kbootargs_pa);
-
-    // // allocate free space for kernel management (e.g., VM tables)
-    // uint32_t top_of_kernel_data_pa = phys_ptr;
-    // printf("Top of kernel data: %08x\n", top_of_kernel_data_pa);
-    // allocate_ram(sysmem, "n45.extra", phys_ptr, 0x300000);
-    // phys_ptr += align_64k_high(0x300000);
-    // uint32_t mem_size = 0x8000000; // TODO hard-coded
-
-    // // load the device tree
-    // macho_load_dtb(nms->dtb_filename, nsas, sysmem, "devicetree.45", phys_ptr, &dtb_size, &nms->uart_mmio_pa);
-    // uint32_t dtb_va = phys_ptr;
-
-    // phys_ptr += align_64k_high(dtb_size);
-
-    // // allocate framebuffer memory
-    //allocate_ram(sysmem, "framebuffer", phys_ptr, align_64k_high(3 * 320 * 480));
-
-    // // load boot args
-    // macho_setup_bootargs("k_bootargs.n45", nsas, sysmem, kbootargs_pa, virt_base, IPOD_TOUCH_PHYS_BASE, mem_size, top_of_kernel_data_pa, dtb_va, dtb_size, nms->kern_args, phys_ptr);
 
     allocate_ram(sysmem, "sram1", SRAM1_MEM_BASE, 0x10000);
 
@@ -683,52 +648,9 @@ static void ipod_touch_memory_setup(MachineState *machine, MemoryRegion *sysmem,
     }
 }
 
-static void ipod_touch_set_kernel_filename(Object *obj, const char *value, Error **errp)
-{
-    IPodTouchMachineState *nms = IPOD_TOUCH_MACHINE(obj);
-    g_strlcpy(nms->kernel_filename, value, sizeof(nms->kernel_filename));
-}
-
-static char *ipod_touch_get_kernel_filename(Object *obj, Error **errp)
-{
-    IPodTouchMachineState *nms = IPOD_TOUCH_MACHINE(obj);
-    return g_strdup(nms->kernel_filename);
-}
-
-static void ipod_touch_set_dtb_filename(Object *obj, const char *value, Error **errp)
-{
-    IPodTouchMachineState *nms = IPOD_TOUCH_MACHINE(obj);
-    g_strlcpy(nms->dtb_filename, value, sizeof(nms->dtb_filename));
-}
-
-static char *ipod_touch_get_dtb_filename(Object *obj, Error **errp)
-{
-    IPodTouchMachineState *nms = IPOD_TOUCH_MACHINE(obj);
-    return g_strdup(nms->dtb_filename);
-}
-
-static void ipod_touch_set_kern_args(Object *obj, const char *value, Error **errp)
-{
-    IPodTouchMachineState *nms = IPOD_TOUCH_MACHINE(obj);
-    g_strlcpy(nms->kern_args, value, sizeof(nms->kern_args));
-}
-
-static char *ipod_touch_get_kern_args(Object *obj, Error **errp)
-{
-    IPodTouchMachineState *nms = IPOD_TOUCH_MACHINE(obj);
-    return g_strdup(nms->kern_args);
-}
-
 static void ipod_touch_instance_init(Object *obj)
 {
-	object_property_add_str(obj, "kernel-filename", ipod_touch_get_kernel_filename, ipod_touch_set_kernel_filename);
-    object_property_set_description(obj, "kernel-filename", "Set the kernel filename to be loaded");
-
-    object_property_add_str(obj, "dtb-filename", ipod_touch_get_dtb_filename, ipod_touch_set_dtb_filename);
-    object_property_set_description(obj, "dtb-filename", "Set the dev tree filename to be loaded");
-
-    object_property_add_str(obj, "kern-cmd-args", ipod_touch_get_kern_args, ipod_touch_set_kern_args);
-    object_property_set_description(obj, "kern-cmd-args", "Set the XNU kernel cmd args");
+	
 }
 
 static inline qemu_irq s5l8900_get_irq(IPodTouchMachineState *s, int n)

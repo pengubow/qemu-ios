9f3e33e3cf1fffc630780b637383f4e2dab39b5e
Martijn de Vos <mdmartijndevos@gmail.com>
2022-07-17 16:00:46 +0200

    Worked on RTC

--- include/hw/arm/ipod_touch_pcf50633_pmu.h
+++ include/hw/arm/ipod_touch_pcf50633_pmu.h
@@ -7,12 +7,22 @@
 #include "hw/sysbus.h"
 #include "hw/i2c/i2c.h"
 #include "hw/irq.h"
+#include "time.h"
 
 #define TYPE_PCF50633                 "pcf50633"
 OBJECT_DECLARE_SIMPLE_TYPE(Pcf50633State, PCF50633)
 
 #define PMU_MBCS1 0x4B
 #define PMU_ADCC1 0x57
+
+// RTC registers
+#define PMU_RTCSC 0x59
+#define PMU_RTCMN 0x5A
+#define PMU_RTCHR 0x5B
+#define PMU_RTCWD 0x5C
+#define PMU_RTCDT 0x5D
+#define PMU_RTCMT 0x5E
+#define PMU_RTCYR 0x5F
 
 typedef struct Pcf50633State {
 	I2CSlave i2c;
